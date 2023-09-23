dbc5bdf59059afb656b3121604e62429bf4169bb
Raiwulf <cibinkanat@gmail.com>
2023-09-23 21:42:00 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,27 @@
+"""
+pyNull - File Nullification Utility
+
+pyNull is a simple file manipulation utility that allows you to nullify (empty) files 
+in a specified directory based on a regular expression filter. 
+It can be used to quickly empty files that match a specific pattern, 
+useful for various data processing tasks.
+
+Usage:
+1. Run the program.
+2. Enter the target directory path where you want to nullify files in the 'Path' field.
+3. Specify a regular expression pattern in the 'RegEx' field to filter files.
+4. Click the 'Browse' button to select the target directory or manually enter the path.
+5. Click the 'Process' button to nullify matching files 
+by creating backups and emptying the original files.
+6. The operation log is displayed in the text box at the bottom.
+
+For more information and updates, visit the project on GitHub:
+https://github.com/Raiwulf/pynull
+
+Author: Raiwulf
+Version: 1.0
+"""
+
 import os
 import re
 import shutil
@@ -15,18 +39,21 @@ CONFIG_FILE = OUTPUT_PATH / "config.txt"
 
 icon = ICON_FOLDER / "icon.ico"
 
+
 def relative_to_assets(path: str) -> Path:
     return FRAME0_PATH / Path(path)
+
 
 def open_file_dialog():
     path = filedialog.askdirectory()
     entry_path.delete(0, tk.END)
     entry_path.insert(0, path)
 
+
 def process_files():
     directory_path = entry_path.get()
     regex_filter = entry_regex.get()
-    backup_folder = os.path.join(directory_path, 'pyNull_Backups')
+    backup_folder = os.path.join(directory_path, "pyNull_Backups")
 
     if not os.path.exists(backup_folder):
         os.makedirs(backup_folder)
@@ -41,7 +68,7 @@ def process_files():
                     if source_file != backup_file:
                         shutil.copy(source_file, backup_file)
 
-                        open(source_file, 'w').close()
+                        open(source_file, "w").close()
 
         entry_log.config(state=tk.NORMAL)
         entry_log.insert(tk.END, "Files nulled successfully!\n")
@@ -51,10 +78,12 @@ def process_files():
         entry_log.insert(tk.END, f"Error: {str(e)}\n")
         entry_log.config(state=tk.DISABLED)
 
+
 def save_data():
     with open(CONFIG_FILE, "w") as file:
         file.write(f"{entry_path.get()}\n")
         file.write(f"{entry_regex.get()}\n")
+
 
 def load_data():
     try:
@@ -71,15 +100,17 @@ def load_data():
         entry_regex.delete(0, tk.END)
         entry_regex.insert(0, "RegEx")
 
+
 def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit the application?"):
         save_data()
         window.destroy()
 
+
 window = Tk()
 
 window.geometry("375x512")
-window.configure(bg = "#FFFFFF")
+window.configure(bg="#FFFFFF")
 window.title("CigiLabs - pyNull")
 window.iconbitmap(icon)
 custom_font = FONT_FOLDER / "Comfortaa-Regular.ttf"
@@ -89,183 +120,87 @@ window.protocol("WM_DELETE_WINDOW", on_closing)
 
 canvas = Canvas(
     window,
-    bg = "#d9d9d9",
-    height = 512,
-    width = 375,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
+    bg="#d9d9d9",
+    height=512,
+    width=375,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge",
 )
 
-canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    187.0,
-    256.0,
-    image=image_image_1
-)
+canvas.place(x=0, y=0)
+image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(187.0, 256.0, image=image_image_1)
 
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    64.0,
-    105.0,
-    image=image_image_2
-)
+image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(64.0, 105.0, image=image_image_2)
 
-button_process_image = PhotoImage(
-    file=relative_to_assets("button_process.png"))
+button_process_image = PhotoImage(file=relative_to_assets("button_process.png"))
 button_process = Button(
     image=button_process_image,
     borderwidth=0,
     highlightthickness=0,
     command=process_files,
-    relief="flat"
+    relief="flat",
 )
-button_process.place(
-    x=32.0,
-    y=336.0,
-    width=315.0,
-    height=58.0
-)
+button_process.place(x=32.0, y=336.0, width=315.0, height=58.0)
 
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    217.0,
-    315.0,
-    image=image_image_3
-)
+image_image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(217.0, 315.0, image=image_image_3)
 
-image_image_4 = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    217.0,
-    221.0,
-    image=image_image_4
-)
+image_image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(217.0, 221.0, image=image_image_4)
 
-entry_regex_image = PhotoImage(
-    file=relative_to_assets("entry_regex.png"))
-entry_regex_bg = canvas.create_image(
-    217.5,
-    284.0,
-    image=entry_regex_image
-)
-entry_regex = Entry(
-    bd=0,
-    bg="#d9d9d9",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_regex.place(
-    x=96.0,
-    y=260.0,
-    width=243.0,
-    height=46.0
-)
+entry_regex_image = PhotoImage(file=relative_to_assets("entry_regex.png"))
+entry_regex_bg = canvas.create_image(217.5, 284.0, image=entry_regex_image)
+entry_regex = Entry(bd=0, bg="#d9d9d9", fg="#000716", highlightthickness=0)
+entry_regex.place(x=96.0, y=260.0, width=243.0, height=46.0)
 
-entry_path_image = PhotoImage(
-    file=relative_to_assets("entry_path.png"))
-entry_path_bg = canvas.create_image(
-    217.5,
-    190.0,
-    image=entry_path_image
-)
-entry_path = Entry(
-    bd=0,
-    bg="#d9d9d9",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_path.place(
-    x=96.0,
-    y=166.0,
-    width=243.0,
-    height=46.0
-)
+entry_path_image = PhotoImage(file=relative_to_assets("entry_path.png"))
+entry_path_bg = canvas.create_image(217.5, 190.0, image=entry_path_image)
+entry_path = Entry(bd=0, bg="#d9d9d9", fg="#000716", highlightthickness=0)
+entry_path.place(x=96.0, y=166.0, width=243.0, height=46.0)
 
-entry_log_image = PhotoImage(
-    file=relative_to_assets("entry_log.png"))
-entry_log_bg = canvas.create_image(
-    189.5,
-    456.5,
-    image=entry_log_image
-)
-entry_log = Text(
-    bd=0,
-    bg="#d9d9d9",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_log.place(
-    x=40.0,
-    y=414.0,
-    width=299.0,
-    height=83.0
-)
+entry_log_image = PhotoImage(file=relative_to_assets("entry_log.png"))
+entry_log_bg = canvas.create_image(189.5, 456.5, image=entry_log_image)
+entry_log = Text(bd=0, bg="#d9d9d9", fg="#000716", highlightthickness=0)
+entry_log.place(x=40.0, y=414.0, width=299.0, height=83.0)
 entry_log.config(state=tk.DISABLED)
 
-image_image_5 = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    56.0,
-    284.0,
-    image=image_image_5
-)
+image_image_5 = PhotoImage(file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(56.0, 284.0, image=image_image_5)
 
-button_folder_image = PhotoImage(
-    file=relative_to_assets("button_folder.png"))
+button_folder_image = PhotoImage(file=relative_to_assets("button_folder.png"))
 button_folder = Button(
     image=button_folder_image,
     borderwidth=0,
     highlightthickness=0,
     command=open_file_dialog,
-    relief="flat"
+    relief="flat",
 )
-button_folder.place(
-    x=32.0,
-    y=166.0,
-    width=48.0,
-    height=48.0
-)
+button_folder.place(x=32.0, y=166.0, width=48.0, height=48.0)
 
-image_image_6 = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    217.0,
-    110.0,
-    image=image_image_6
-)
+image_image_6 = PhotoImage(file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(217.0, 110.0, image=image_image_6)
 
 canvas.create_text(
-    38.0,
-    192.0,
-    anchor="nw",
-    text="",
-    fill="#FFFFFF",
-    font=("Comfortaa Bold", 16 * -1)
+    38.0, 192.0, anchor="nw", text="", fill="#FFFFFF", font=("Comfortaa Bold", 16 * -1)
 )
+
 
 def open_github_url():
     webbrowser.open_new("https://github.com/Raiwulf/pynull")
 
-button_info_image = PhotoImage(
-    file=relative_to_assets("button_info.png"))
+
+button_info_image = PhotoImage(file=relative_to_assets("button_info.png"))
 button_info = Button(
     image=button_info_image,
     borderwidth=0,
     highlightthickness=0,
     command=open_github_url,
-    relief="flat"
+    relief="flat",
 )
-button_info.place(
-    x=335.0,
-    y=0.0,
-    width=40.0,
-    height=40.0
-)
+button_info.place(x=335.0, y=0.0, width=40.0, height=40.0)
 
 load_data()
 
